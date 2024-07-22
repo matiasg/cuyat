f3b4aa95ec6491418b65f302096d22c7cbd4d33b
Matías Graña <matias.alejo@gmail.com>
2024-07-22 23:33:43 +0200

    add a panel with fixed image

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,14 @@ use cursive::view::View;
 use cursive::{Printer, Vec2};
 use nalgebra::UnitQuaternion;
 
-use cuyat::{FoV, Sky, Star};
+use cuyat::{FoV, Sky};
 
 #[derive(Clone)]
 struct SkyView {
     pub sky: Sky,
     fov: FoV,
     q: UnitQuaternion<f32>,
+    margin: usize,
 }
 
 impl SkyView {
@@ -19,37 +20,59 @@ impl SkyView {
         let q: UnitQuaternion<f32> = UnitQuaternion::default();
         let sky = Sky::random_with_stars(nstars);
         let fov = FoV::new(2.0, 2.0);
-        Self { sky, fov, q }
+        Self {
+            sky,
+            fov,
+            q,
+            margin: 1,
+        }
     }
 
     fn rotate(&mut self, x: f32, y: f32, z: f32) {
         self.q = UnitQuaternion::from_euler_angles(x * 0.1, y * 0.1, z * 0.1) * self.q;
+    }
+
+    fn draw_portion(&self, quat: UnitQuaternion<f32>, p: &Printer, x_max: u8, y_max: u8) {
+        let style = ColorStyle::new(Color::Rgb(255, 255, 255), Color::Rgb(0, 0, 64));
+
+        for (i, fps) in self
+            .fov
+            .project_sky_to_screen(self.sky.with_attitude(quat), x_max, y_max)
+            .iter()
+            .enumerate()
+            .filter(|(_, p)| p.is_some())
+        {
+            p.with_color(style, |printer| {
+                printer.print(
+                    (fps.unwrap().0, fps.unwrap().1),
+                    std::str::from_utf8(&[i as u8 + 97]).unwrap(),
+                );
+            });
+        }
     }
 }
 
 impl View for SkyView {
     fn draw(&self, p: &Printer) {
         let x_max = p.size.x as u8;
+        let x_mid = x_max / 2;
         let y_max = p.size.y as u8;
 
-        let style = ColorStyle::new(Color::Rgb(255, 255, 255), Color::Rgb(0, 0, 64));
+        let left = cursive::Vec2::new(1, 1);
+        let left_printer = p.offset(left);
+        self.draw_portion(self.q, &left_printer, x_mid, y_max);
 
-        for (i, fps) in self
-            .fov
-            .project_sky_to_screen(self.sky.with_attitude(self.q), x_max, y_max)
-            .iter()
-            .enumerate()
-        {
-            p.with_color(style, |printer| {
-                printer.print(
-                    (fps.0, fps.1),
-                    std::str::from_utf8(&[i as u8 + 97]).unwrap(),
-                );
-            });
+        let style = ColorStyle::new(Color::Rgb(20, 200, 200), Color::Rgb(0, 0, 0));
+        for y in 0..y_max {
+            p.with_color(style, |printer| printer.print((x_mid, y), "|"))
         }
+
+        let right = cursive::Vec2::new(x_mid as usize + self.margin, 1);
+        let right_printer = p.offset(right);
+        self.draw_portion(UnitQuaternion::default(), &right_printer, x_mid, y_max);
     }
     fn required_size(&mut self, _constraint: Vec2) -> Vec2 {
-        Vec2::new(60, 32)
+        Vec2::new(121, 32)
     }
 
     fn on_event(&mut self, event: Event) -> EventResult {
