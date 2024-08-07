55b8a409d44acc963ee9b684417d6f9ae12a3ce0
Matías Graña <matias.alejo@gmail.com>
2024-08-08 00:38:39 +0200

    remove comment in main

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
 use cuyat::view::SkyView;
 
 fn main() {
-    // for random sky use this
-    // let (sky_view, score_rc) = SkyView::new(None, 200);
     let (sky_view, score_rc) = SkyView::new(Some(String::from("bsc5.csv")), 400);
     let mut siv = cursive::default();
     siv.add_layer(sky_view);
