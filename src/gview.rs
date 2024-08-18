151ba0e99a1d908207fb7712413482d78c101d2c
Matías Graña <matias.alejo@gmail.com>
2024-08-19 00:13:42 +0200

    draw bottom half

--- src/gview.rs
+++ src/gview.rs
@@ -1,4 +1,4 @@
-use std::{cell::RefCell, rc::Rc};
+use std::{borrow::BorrowMut, cell::RefCell, rc::Rc};
 
 use macroquad::prelude::*;
 use nalgebra::UnitQuaternion;
@@ -21,8 +21,8 @@ pub struct GSkyView {
 impl GSkyView {
     pub fn new(scoring: Rc<RefCell<Scoring>>) -> Self {
         let catalog = Some("bsc5.csv".to_string());
-        let target_q = random_quaternion();
         let nstars: usize = 1200;
+        let target_q = random_quaternion();
         let sky = Sky::new(&catalog, nstars).with_attitude(target_q);
         let options = Options {
             show_distance: false,
@@ -43,34 +43,57 @@ impl GSkyView {
             options,
         }
     }
-
+    fn make_sky(&mut self) {
+        self.sky = Sky::new(&self.options.catalog_filename, self.options.nstars)
+            .with_attitude(self.target_q);
+    }
     pub fn options(&self) -> &Options {
         &self.options
     }
-
     fn rotate(&mut self, x: f32, y: f32, z: f32) {
         self.real_q =
             UnitQuaternion::from_euler_angles(x * self.step, y * self.step, z * self.step)
                 * self.real_q;
         (*self.scoring).borrow_mut().add_move();
     }
-
-    fn draw_portion(&self, quat: UnitQuaternion<f32>, x_max: u8, y_max: u8) {
+    fn draw_portion(
+        &self,
+        quat: UnitQuaternion<f32>,
+        x_min: f32,
+        x_max: f32,
+        y_min: f32,
+        y_max: f32,
+    ) {
+        let width = (x_max - x_min) * 256.0;
+        let height = (y_max - y_min) * 256.0;
         for fps in self
             .fov
-            .project_sky_to_screen(self.sky.with_attitude(quat), x_max, y_max)
+            .project_sky_to_screen(self.sky.with_attitude(quat), width as u8, height as u8)
             .into_iter()
             .flatten()
         {
-            let (px, py, b, n) = fps;
-            let px = (px as f32) / (x_max as f32) * screen_width();
-            let py = (py as f32) / (y_max as f32) * screen_height();
+            let (px, py, _, n) = fps;
+            let px = (x_min + (px as f32) / 256.0) * screen_width();
+            let py = (y_min + (py as f32) / 256.0) * screen_height();
             if self.options.show_star_names {
                 draw_text(&n, px, py, 18.0, WHITE);
             } else {
                 draw_circle(px, py, 4.0, WHITE);
             }
         }
+    }
+    fn distance(&self) -> f32 {
+        let (roll, pitch, yaw) = (self.target_q / self.real_q).euler_angles();
+        (roll.powi(2) + pitch.powi(2) + yaw.powi(2)).sqrt()
+    }
+    fn restart(&mut self) {
+        (*self.scoring)
+            .borrow_mut()
+            .score_and_reset(self.distance());
+        self.target_q = random_quaternion();
+        self.make_sky();
+        self.real_q = random_quaternion();
+        self.step = 0.5;
     }
 }
 
@@ -102,13 +125,25 @@ pub async fn main() {
         if is_key_pressed(KeyCode::V) {
             view.options.show_star_names = !view.options.show_star_names;
         }
+        if is_key_pressed(KeyCode::Space) {
+            view.restart();
+        }
 
         if is_key_pressed(KeyCode::Q) {
             break;
         }
 
         clear_background(BLACK);
-        view.draw_portion(view.real_q, 80, 80);
+        view.draw_portion(view.real_q, 0.0, 1.0, 0.0, 0.5);
+        draw_line(
+            0.0,
+            screen_height() / 2.0,
+            screen_width(),
+            screen_height() / 2.0,
+            2.0,
+            YELLOW,
+        );
+        view.draw_portion(view.target_q, 0.0, 1.0, 0.5, 1.0);
 
         let header_1 = format!(
             "Stars: {}, catalog: {}. Step: {:.4}, zoom: {:.3}, moves: {}, games: {}, score: {:.6}",
@@ -123,13 +158,8 @@ pub async fn main() {
             (*view.scoring).borrow().total.len(),
             (*view.scoring).borrow().get_score(),
         );
-        draw_text(&header_1, 10.0, 10.0, 18.0, GRAY);
-        draw_text(&quat_coords_str(view.real_q), 10.0, 30.0, 18.0, GRAY);
-
-        // draw_line(40.0, 40.0, 100.0, 200.0, 15.0, BLUE);
-        // draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
-        // draw_circle(screen_width() - 30.0, screen_height() - 30.0, 15.0, YELLOW);
-        // draw_text("HELLO", 20.0, 20.0, 20.0, DARKGRAY);
+        draw_text(&header_1, 10.0, 20.0, 18.0, GRAY);
+        draw_text(&quat_coords_str(view.real_q), 10.0, 38.0, 18.0, GRAY);
 
         next_frame().await
     }
